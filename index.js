3632255e14da04035b998e0736071a6739cf7209
MrUnikPrince <opuniqueprince@gmail.com>
2023-07-17 02:08:56 +0530

    running my app on server

---
+++ index.js
@@ -0,0 +1,14 @@
+const { log } = require("console");
+const express = require("express");
+const port = 8000;
+
+const app = express();
+
+
+// setting up server
+app.listen(port, (err) =>{
+    if(err){
+        log(`Error in running server ${err}`);
+    }
+    log(`Server is runninng on port ${port}`);
+})

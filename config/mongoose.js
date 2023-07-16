d09a25cea989fa8a347615c02fee6d6339b04f26
MrUnikPrince <opuniqueprince@gmail.com>
2023-07-17 02:23:48 +0530

    setting connection with database

---
+++ config/mongoose.js
@@ -0,0 +1,9 @@
+const { log } = require("console");
+const mongoose = require("mongoose");
+
+mongoose.connect("mongodb://localhost/codial",{useNewUrlParser: true,
+useUnifiedTopology: true,})
+.then(() => log("Connected to mongoDB"))
+.catch((err) => log(`Error in connecting to mongoDb ${err}`));
+
+// module.exports = db;

0d41cafb5b207bdf26a426dd18ab9ee24e2ca8f7
MrUnikPrince <opuniqueprince@gmail.com>
2023-07-23 05:42:40 +0530

    scss setup done

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const { log } = require('console');
 const express = require('express');
 const cookieParser = require('cookie-parser');
-const port = 8000;
+const port = 9000;
 const expressLayouts = require('express-ejs-layouts');
 const db = require('./config/mongoose');
 // express app
@@ -12,7 +12,15 @@ const session = require('express-session');
 const passport = require('passport');
 const passportLocal = require('./config/passport-local-strategy');
 const MongoStore = require('connect-mongo');
+const sassMiddleware = require('node-sass-middleware');
 
+app.use(sassMiddleware({
+    src : './assets/scss',
+    dest: './assets/css',
+    debug: true,
+    outputStyle: 'extended',
+    prefix: './css'
+}));
 
 app.use(express.urlencoded({ extended: true }));
 

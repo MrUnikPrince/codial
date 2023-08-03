7b1505e85de08aee435224cd2788a2c6f7e6186c
MrUnikPrince <opuniqueprince@gmail.com>
2023-08-04 05:06:30 +0530

    user can delete post and associated comments as well

--- controllers/post_controller.js
+++ controllers/post_controller.js
@@ -1,4 +1,5 @@
 const Post = require('../models/post');
+const Comment = require('../models/comment');
 
 // Post create
 module.exports.create = async (req, res) => {
@@ -13,3 +14,24 @@ module.exports.create = async (req, res) => {
       return res.redirect('/users/sign-in');
    }
 };
+
+// Post delete
+module.exports.destroy = async (req, res) => {
+   try{
+      const post = await Post.findById(req.params.id);
+      if(post.user == req.user.id){
+         // Delete the post
+         await Post.deleteOne({ _id: req.params.id });
+
+         // delete associated comments
+         const deletedComment = await Comment.deleteMany({post: req.params.id});
+         if(deletedComment){
+            return res.redirect('back')
+         }
+      }else{
+         return res.redirect('back');
+      }
+   }catch(err){
+      console.log(`Error in deleting user's post ${err}`);
+   }
+}
--- routes/posts.js
+++ routes/posts.js
@@ -3,7 +3,8 @@ const passport = require('passport');
 const postController = require('../controllers/post_controller');
 
 // for Creating post
-
 router.post('/create', passport.checkAuthentication, postController.create);
 
+// For deleting the post
+router.get('/destroy/:id', passport.checkAuthentication, postController.destroy);
 module.exports = router;
